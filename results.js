21501bae2b158f023542f9a2fa3413bc7a8997f8
ileya-creator <maryo.shado@gmail.com>
2026-02-28 06:51:05 +0330

    Add files via upload

---
+++ results.js
@@ -0,0 +1,18 @@
+// نتایج مسابقات جام رمضان
+const matchResults = {
+    // بازی کلاس 11 و 16
+    "11-16": { homeScore: 1, awayScore: 1, adminNote: "مساوی" },
+    
+    // بقیه بازی‌ها (فعلاً خالی)
+    "14-18": { homeScore: 0, awayScore: 0, adminNote: "" },
+    "15-1": { homeScore: 0, awayScore: 0, adminNote: "" },
+    "9-12": { homeScore: 0, awayScore: 0, adminNote: "" },
+    "17-16": { homeScore: 0, awayScore: 0, adminNote: "" },
+    "18-5": { homeScore: 0, awayScore: 0, adminNote: "" },
+    "15-7": { homeScore: 0, awayScore: 0, adminNote: "" },
+    "9-13": { homeScore: 0, awayScore: 0, adminNote: "" },
+    "11-17": { homeScore: 0, awayScore: 0, adminNote: "" },
+    "14-5": { homeScore: 0, awayScore: 0, adminNote: "" },
+    "1-7": { homeScore: 0, awayScore: 0, adminNote: "" },
+    "12-13": { homeScore: 0, awayScore: 0, adminNote: "" }
+};
